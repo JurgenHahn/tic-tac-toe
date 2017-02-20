6e9915051c4843c4145d2d196162de58c2798255
JurgenHahn <jurgenehahn@gmail.com>
2017-02-19 23:48:34 -0500

    added a draw condition, improved UX experience with board redesign

--- js/main.js
+++ js/main.js
@@ -1,93 +1,97 @@
 $('.box').on('click', function(){
-
   // $('body').toggleClass('O');
-    var space = $(this).text().trim();
-    var player = $('body').attr('class');
-    var unused = "";
-    var notOver = $(this).attr('class') === 'box';
+  var space = $(this).text().trim();
+  var player = $('body').attr('class');
+  var unused = "";
 
-
-    if (player === 'X' && space === unused && notOver) {
-      $(this).text('X');
-      $('body').attr('class', 'O');
-      winCondition();
-
-    }
-    else if (player === 'O' && space === unused && notOver) {
-      $(this).text('O');
-      $('body').attr('class', 'X');
-      winCondition();
-
-    }
-});
-
-
-
-
-function reset(){
-  $('body').attr('class', 'X');
-  $('.filled').attr('class', 'box');
-  $('.box').empty();
-  alert("Game Over. Thanks for Playing!");
-};
-
-
-function winCondition(){
-
-  var topLeft = $('.box')[0].textContent;
-  var topMiddle = $('.box')[1].textContent;
-  var topRight = $('.box')[2].textContent;
-  var middleLeft = $('.box')[3].textContent;
-  var middleMiddle = $('.box')[4].textContent;
-  var middleRight = $('.box')[5].textContent;
-  var bottomLeft = $('.box')[6].textContent;
-  var bottomMiddle = $('.box')[7].textContent;
-  var bottomRight = $('.box')[8].textContent;
-
-  if ((topLeft === "X" || topLeft === "O") &&
-    ((topLeft === topMiddle && topLeft === topRight) ||
-    (topLeft === middleLeft && topLeft === bottomLeft) ||
-    (topLeft === middleMiddle && topLeft === bottomRight))){
-    // console.log("Winner!");
-    $('.box').attr("class", "filled");
-    setTimeout(reset, 2500);
-
-  }
-
-  else if ((topRight === "X" || topRight === "O") &&
-    ((topRight === middleRight && topLeft === bottomRight) ||
-    (topRight === middleMiddle && topRight === bottomLeft))){
-    // console.log("Winner!");
-    $('.box').attr("class", "filled");
-    setTimeout(reset, 2500);
-  }
-
-  else if ((middleMiddle === "X" || middleMiddle === "O") &&
-    ((middleMiddle === topMiddle && middleMiddle === bottomMiddle) ||
-    (middleMiddle === middleRight && middleMiddle === middleLeft))){
-    // console.log("Winner!");
-    $('.box').attr("class", "filled");
-    setTimeout(reset, 2500);
-  }
-
-  else if ((bottomMiddle === "X" || bottomMiddle === "O") &&
-  (bottomMiddle === bottomRight && bottomMiddle === bottomLeft)){
-    // console.log("Winner!");
-    $('.box').attr("class", "filled");
-    setTimeout(reset, 2500);
-  }
-
-  else if (
-    (topLeft === "X" || topLeft === "0") &&
-    (topMiddle === "X" || topMiddle === "0") &&
-    (topRight === "X" || topRight === "0") &&
-    (middleLeft === "X" || middleLeft === "0") &&
-    (middleMiddle === "X" || middleMiddle === "0") &&
-    (middleRight === "X" || middleRight === "0") &&
-    (bottomLeft === "X" || bottomLeft === "0") &&
-    (bottomMiddle === "X" || bottomMiddle === "0") &&
-    (bottomRight === "X" || bottomRight === "0")
-    ){
-    setTimeout(reset, 2500);
+  if (player === 'X' && space === unused) {
+    $(this).text('X');
+    $('body').attr('class', 'O');
+  } else if (player === 'O' && space === unused) {
+    $(this).text('O');
+    $('body').attr('class', 'X');
   };
-};
+  winCondition();
+  draw();
+
+  function winCondition(){
+
+    var topLeft = $('.box')[0].textContent;
+    var topMiddle = $('.box')[1].textContent;
+    var topRight = $('.box')[2].textContent;
+    var middleLeft = $('.box')[3].textContent;
+    var middleMiddle = $('.box')[4].textContent;
+    var middleRight = $('.box')[5].textContent;
+    var bottomLeft = $('.box')[6].textContent;
+    var bottomMiddle = $('.box')[7].textContent;
+    var bottomRight = $('.box')[8].textContent;
+
+    if ((topLeft === "X" || topLeft === "O") &&
+      ((topLeft === topMiddle && topLeft === topRight) ||
+      (topLeft === middleLeft && topLeft === bottomLeft) ||
+      (topLeft === middleMiddle && topLeft === bottomRight))){
+      setTimeout(reset, 300);
+    }
+
+    else if ((topRight === "X" || topRight === "O") &&
+      ((topRight === middleRight && topRight === bottomRight) ||
+      (topRight === middleMiddle && topRight === bottomLeft))){
+      setTimeout(reset, 300);
+    }
+
+    else if ((middleMiddle === "X" || middleMiddle === "O") &&
+      ((middleMiddle === topMiddle && middleMiddle === bottomMiddle) ||
+      (middleMiddle === middleRight && middleMiddle === middleLeft))){
+      setTimeout(reset, 300);
+    }
+
+    else if ((bottomMiddle === "X" || bottomMiddle === "O") &&
+      (bottomMiddle === bottomRight && bottomMiddle === bottomLeft)){
+      setTimeout(reset, 300);
+    };
+  };
+
+  function draw(){
+
+    var topLeft = $('.box')[0].textContent;
+    var topMiddle = $('.box')[1].textContent;
+    var topRight = $('.box')[2].textContent;
+    var middleLeft = $('.box')[3].textContent;
+    var middleMiddle = $('.box')[4].textContent;
+    var middleRight = $('.box')[5].textContent;
+    var bottomLeft = $('.box')[6].textContent;
+    var bottomMiddle = $('.box')[7].textContent;
+    var bottomRight = $('.box')[8].textContent;
+
+    if (
+      (topLeft === "X"      || topLeft === "O")       &&
+      (topMiddle === "X"    || topMiddle === "O")     &&
+      (topRight === "X"     || topRight === "O")      &&
+      (middleLeft === "X"   || middleLeft === "O")    &&
+      (middleMiddle === "X" || middleMiddle === "O")  &&
+      (middleRight === "X"  || middleRight === "O")   &&
+      (bottomLeft === "X"   || bottomLeft === "O")    &&
+      (bottomMiddle === "X" || bottomMiddle === "O")  &&
+      (bottomRight === "X"  || bottomRight === "O ")
+    ){
+    setTimeout(reset2, 300);
+    };
+  };
+
+  function reset(){
+    if (player === 'X') {
+      alert("X Wins! X gun give it to ya!");
+    } else {
+      alert("O Wins! OOOOOOOOO Ya!");
+    };
+    $('body').attr('class', 'X');
+    $('.box').empty();
+  };
+
+  function reset2(){
+    alert("It's a Draw! You're both equally bad!");
+    $('.box').empty();
+    $('body').attr('class', 'X');
+  }
+
+});
